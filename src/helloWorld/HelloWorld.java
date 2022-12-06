a09781b92df19379578585899ce0b502f8a161b9
RoscaGh <rosca.gh1984@gmail.com>
2022-12-06 10:51:43 +0200

    Add first project Hello

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HelloWorld {
+    public HelloWorld() {
+    }
+
+    public static void main() throws IOException {
+        System.out.print("Enter your name ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        System.out.println("Hello "+name+"!");
+    }
+}
